cf21926530c70adb22f85cda84da40268d03e4f0
Robin Smith <robin@rsmith.io>
2018-05-16 19:00:49 +0100

    Quick-save.

--- jobs.rb
+++ jobs.rb
@@ -4,7 +4,10 @@ require 'simple-rss'
 require 'open-uri'
 require 'colorize'
 
-Url = ARGV[0]
+Urls = [
+  "https://nodesk.co/index.xml",
+  # "https://angel.co/index.xml"
+]
 
 class NoDeskJobsFilter
   def apply(item)
@@ -22,30 +25,31 @@ end
 
 FeedLimit = ARGV[1].nil? ? 10000 : ARGV[1]
 
-open(Url) do |rss|
-  feed = SimpleRSS.parse open(Url)
-  feed.channel.title.length.times { printf '-'.white }; puts
-  printf "#{feed.channel.title}\n".cyan
-  feed.channel.title.length.times { printf '-'.white }; puts; puts
-
-  (feed.items.select {|item| item_allowed(item, UrlFilters)}).sort_by(&:pubDate)[0...FeedLimit].each_with_index do |item, index|
-    printf '['.cyan << "#{index}".white << '] '.cyan
-    puts "#{item.title} - #{item.pubDate}".white
-    puts "#{item.link}".yellow
-    puts
-  end
-
-  printf "Choose article: ".cyan
-  while (choice = $stdin.gets.chomp) do
-    if (choice.numeric?)
-      `open -a #{ENV['BROWSER_APP']} #{feed.items[choice.to_i].link}`
-    elsif (choice.empty?)
-      break;
-    else
-      puts "Must choose a number within range of options.".red
+# Urls.each do |url|
+  url = Urls[0]
+  open(url) do |rss|
+    feed = SimpleRSS.parse open(url)
+    feed.channel.title.length.times { printf '-'.white }; puts
+    printf "#{feed.channel.title}\n".cyan
+    feed.channel.title.length.times { printf '-'.white }; puts; puts
+    
+    (feed.items.select {|item| item_allowed(item, UrlFilters)}).sort_by(&:pubDate)[0...FeedLimit].each_with_index do |item, index|
+      printf '['.cyan << "#{index}".white << '] '.cyan
+      puts "#{item.title} - #{item.pubDate}".white
+      puts "#{item.link}".yellow
+      puts
     end
+    
     printf "Choose article: ".cyan
+    while (choice = $stdin.gets.chomp) do
+      if (choice.numeric?)
+        `open -a #{ENV['BROWSER_APP']} #{feed.items[choice.to_i].link}`
+      elsif (choice.empty?)
+        break;
+      else
+        puts "Must choose a number within range of options.".red
+      end
+      printf "Choose article: ".cyan
+    end
   end
-end
-
-
+# end
